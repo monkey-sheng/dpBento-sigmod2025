da6a91cf1b64eed9d94fc19779585ca97b93f268
zhumengzhiren <york.chen@mail.utoronto.ca>
2024-10-02 14:18:02 +0800

    no need of json

--- benchmarks/KVS/run.py
+++ benchmarks/KVS/run.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import json
 
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
@@ -13,7 +12,6 @@ from benchmarks.packages.kvs_runner import KVSRunner
 if __name__ == '__main__':
     parser = KVSParser()
     args = parser.parse_arguments()
-    args.operation_type = json.loads(args.operation_type)
-    
+
     kvs_runner = KVSRunner(args)
     kvs_runner.run_benchmark_test(args.operation_size,args.operation_type,args.data_distribution_type)
