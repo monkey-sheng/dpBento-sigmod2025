efec0013d328fbddeb88511bfeb6a500d7026519
zhumengzhiren <york.chen@mail.utoronto.ca>
2024-10-02 13:37:34 +0800

    new error fixed

--- benchmarks/KVS/run.py
+++ benchmarks/KVS/run.py
@@ -10,15 +10,9 @@ from benchmarks.packages.kvs_parser import KVSParser
 from benchmarks.packages.kvs_runner import KVSRunner
 
 if __name__ == '__main__':
-    # kvs_parser = KVSParser()
-    # args = kvs_parser.parse_arguments()
     parser = KVSParser()
-    # 模拟参数
     args = parser.parse_arguments()
     
-    
-    runner = KVSRunner(args)
-    runner.run()
 
     kvs_runner = KVSRunner(args)
     kvs_runner.run_benchmark_test(args.operation_size,args.operation_type,args.data_distribution_type,args.metric)
