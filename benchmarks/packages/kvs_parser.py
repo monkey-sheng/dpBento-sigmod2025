3525c370cadc14987328afddda550d96527c3548
zhumengzhiren <york.chen@mail.utoronto.ca>
2024-10-02 13:44:57 +0800

    load type

--- benchmarks/packages/kvs_parser.py
+++ benchmarks/packages/kvs_parser.py
@@ -1,5 +1,5 @@
 from .parser import Parser
-import json
+
 
 class KVSParser(Parser):
     def __init__(self):
@@ -14,8 +14,6 @@ class KVSParser(Parser):
         self.parser.add_argument("--operation_type_description", help="Description of operation types")
         self.parser.add_argument("--data_distribution_type_description", help="Description of data distribution types")
         self.parser.add_argument("--metrics", nargs='+', help="Metrics to collect, e.g., latency, runtime, throughput")
-        self.operation_type = json.loads(self.operation_type)
-
 
     def parse_arguments(self):
         return self.parser.parse_args()
