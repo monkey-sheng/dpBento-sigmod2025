42f00605b1c202548318f72987951e3b51cc6316
zhumengzhiren <york.chen@mail.utoronto.ca>
2024-10-02 13:34:50 +0800

    fix

--- benchmarks/KVS/run.py
+++ benchmarks/KVS/run.py
@@ -13,21 +13,9 @@ if __name__ == '__main__':
     # kvs_parser = KVSParser()
     # args = kvs_parser.parse_arguments()
     parser = KVSParser()
-    parser.parser.add_argument('--operation_size', type=int, choices=[100000, 300000, 1000000], required=True)
-    parser.parser.add_argument('--operation_type', type=dict, required=True, help='Operation types with proportions.')
-    parser.parser.add_argument('--data_distribution_type', type=str, choices=['zipfian', 'uniform', 'latest'], required=True)
-    parser.parser.add_argument('--metrics', nargs='+', help='Metrics to collect.', default=['runtime', 'latency', 'throughput'])
-
     # 模拟参数
     args = parser.parse_arguments()
-    args.operation_size = 1000000
-    args.operation_type = {
-        'readproportion': 0.4,
-        'updateproportion': 0.4,
-        'insertproportion': 0.1,
-        'scanproportion': 0.1
-    }
-    args.data_distribution_type = 'latest'
+    
     
     runner = KVSRunner(args)
     runner.run()
