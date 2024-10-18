0c5a6966b5ffae1a66996491631295049c0b937d
Jason Hu <hujason@hotmail.com>
2024-10-18 02:20:33 -0400

    use tflite-support==0.4.3

--- benchmarks/ml/prepare.py
+++ benchmarks/ml/prepare.py
@@ -5,7 +5,7 @@ import subprocess
 
 def install_dependencies():
     subprocess.run(r"sudo apt install ffmpeg libsm6 libxext6  -y;\
-                   pip3 install tflite-support opencv-python tflite-support protobuf", shell=True, check=True)
+                   pip3 install tflite-support==0.4.3 opencv-python", shell=True, check=True)
 
 if __name__ == '__main__':
     install_dependencies()
